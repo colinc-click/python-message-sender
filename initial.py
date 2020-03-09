4ab94d626b26453290fcf220d3816aab851599a0
Colin Chumak <chumakc@pm.me>
2020-03-09 14:53:12 -0400

    Updated work

--- initial.py
+++ initial.py
@@ -1,1 +1,54 @@
-print('hello')
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+
+
+def simple_get(url):
+    try:
+        with closing(get(url, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+
+    except RequestException as e:
+        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+        return None
+
+
+def is_good_response(resp):
+    content_type = resp.headers['Content-Type'].lower()
+    return (resp.status_code == 200 and content_type is not None
+            and content_type.find('html') > -1)
+
+
+def log_error(e):
+    print(e)
+
+
+def get_stats():
+    url = 'https://www.nhl.com/player/phil-kessel-8473548'
+    response = simple_get(url)
+
+    if response is not None:
+        html = BeautifulSoup(response, 'html.parser')
+        names = set()
+        for li in html.find_all('div', class_="player-overview__bio"):
+            for name in li.text.split('\n'):
+                if len(name) > 0:
+                    names.add(name.strip())
+        return list(names)
+
+    # Raise an exception if we failed to get any data from the url
+    raise Exception('Error retrieving contents at {}'.format(url))
+
+
+if __name__ == '__main__':
+    print('Getting the list of names....')
+    names = get_stats()
+    print('... done.\n')
+
+    print('\nPhilly boys stats...:\n')
+    for name in names:
+        print(name)
